856472ec435926d5da2f4b3be7a3c217486a0a50
bader-waseem <25441886+bader-waseem@users.noreply.github.com>
2020-09-29 12:15:14 -0600

    Final submission version 1 with "working" scene editor

--- 453-skeleton/453-skeleton/453-skeleton/main.cpp
+++ 453-skeleton/453-skeleton/453-skeleton/main.cpp
@@ -16,10 +16,13 @@
 #include "Window.h"
 
 
+// Scene 1 = Serpinsky Triangle, Scene 2 = Square Diamond, Scene 3 = Koch Snowflake
 struct State {
-	int segments = 4;
+	// There is this really weird bug where it doesn't draw at the iteration it is set to and I have no clue why it does that
+	int iterations = 0;
+	int scene = 1;
 	bool operator == (State const& other) const {
-		return segments == other.segments;
+		return iterations == other.iterations;
 	}
 };
 
@@ -41,15 +44,34 @@ public:
 				shader.recompile();
 			}
 			if (key == GLFW_KEY_LEFT) {
-				if (state.segments > 0) {
-					state.segments--;
+				if (state.iterations > 0) {
+					state.iterations--;
 				}
 			}
 			if (key == GLFW_KEY_RIGHT) {
-				if (state.segments < 1024) {
-					state.segments++;
+				if (state.iterations < 10) {
+					state.iterations++;
 				}
 			}
+
+			if (key == GLFW_KEY_1) {
+				if (state.scene != 1) {
+					state.scene = 1;
+				}
+			}
+
+			if (key == GLFW_KEY_2) {
+				if (state.scene != 2) {
+					state.scene = 2;
+				}
+			}
+
+			if (key == GLFW_KEY_3) {
+				if (state.scene != 3) {
+					state.scene = 3;
+				}
+			}
+
 		}
 	}
 	State getState() {
@@ -70,6 +92,7 @@ glm::vec3 const RED = glm::vec3(1.f, 0.f, 0.f);
 glm::vec3 const GREEN = glm::vec3(0.f, 1.f, 0.f);
 glm::vec3 const BLUE = glm::vec3(0.f, 0.f, 1.f);
 glm::vec3 const BLACK = glm::vec3(0.f, 0.f, 0.f);
+glm::vec3 const YELLOW = glm::vec3(1.f, 1.f, 0.f);
 
 // Helper function for entering points as data
 glm::vec3 point(std::vector<float> point) {
@@ -179,35 +202,45 @@ void generateSquareDiamond(CPU_Geometry& squareDiamond, int iterations, std::vec
 }
 
 // Function to generate geometry for koch snowflake
-void generateSnowflake(CPU_Geometry& snowflake, std::vector<float> startingPoint, std::vector<float> endingPoint, int iterations) {
+void generateSnowflake(CPU_Geometry& snowflake, std::vector<float> startingPoint, std::vector<float> endingPoint, glm::vec3 color, int iterations) {
 	float firstPointAlpha = 1.f / 3.f;
 	float lastPointAlpha = 2.f / 3.f;
 	float endPointAlpha = 1.f;
 	if (iterations > 0) {
 		std::vector<float> firstPoint = pointOnLine(firstPointAlpha, startingPoint, endingPoint);
 		std::vector<float> lastPoint = pointOnLine(lastPointAlpha, startingPoint, endingPoint);
-		std::vector<float> firstLastVector = getVectorFromPoints(firstPoint, lastPoint);
-
-		glm::mat4 rotationMat(1); // Creates identity matrix
-
-		// Rotation referenced here https://stackoverflow.com/questions/20923232/how-to-rotate-a-vector-by-a-given-direction
-		rotationMat = glm::rotate(rotationMat, 60.0f, glm::vec3(firstLastVector[0], firstLastVector[1], 0.f));
-		glm::vec3 rotatedVector = glm::vec3(rotationMat * glm::vec4(glm::vec3(firstLastVector[0], firstLastVector[1], 0.f), 1.0));
-
-		std::vector<float> middle = getPointFromVector(firstPoint, rotatedVector);
+		std::vector<float> middle = rotatePoint(firstPoint, lastPoint, 60.0);
 
 
-		generateSnowflake(snowflake, startingPoint, firstPoint, iterations - 1);
-		generateSnowflake(snowflake, firstPoint, middle, iterations - 1);
-		generateSnowflake(snowflake, middle, lastPoint, iterations - 1);
-		generateSnowflake(snowflake, lastPoint, endingPoint, iterations - 1);
+		generateSnowflake(snowflake, startingPoint, firstPoint, BLUE,iterations - 1);
+		generateSnowflake(snowflake, firstPoint, middle, GREEN, iterations - 1);
+		generateSnowflake(snowflake, middle, lastPoint, RED, iterations - 1);
+		generateSnowflake(snowflake, lastPoint, endingPoint, YELLOW, iterations - 1);
 	}
 	else {
 		snowflake.verts.push_back(point(startingPoint));
 		snowflake.verts.push_back(point(endingPoint));
+		snowflake.cols.push_back(color);
+		snowflake.cols.push_back(color);
 	}
 }
 
+void clearScene(CPU_Geometry& triangles, CPU_Geometry& squareDiamond, CPU_Geometry& snowflake1, CPU_Geometry& snowflake2, CPU_Geometry& snowflake3) {
+	triangles.verts.clear();
+	triangles.cols.clear();
+
+	squareDiamond.verts.clear();
+	squareDiamond.cols.clear();
+
+	snowflake1.verts.clear();
+	snowflake1.cols.clear();
+
+	snowflake2.verts.clear();
+	snowflake2.cols.clear();
+
+	snowflake3.verts.clear();
+	snowflake3.cols.clear();
+}
 
 int main() {
 	Log::debug("Starting main");
@@ -228,8 +261,14 @@ int main() {
 	// GEOMETRY
 	CPU_Geometry triangles;
 	GPU_Geometry trianglesGPU;
-	CPU_Geometry snowflake;
-	GPU_Geometry snowflakeGPU;
+
+	CPU_Geometry snowflake1;
+	GPU_Geometry snowflake1GPU;
+	CPU_Geometry snowflake2;
+	GPU_Geometry snowflake2GPU;
+	CPU_Geometry snowflake3;
+	GPU_Geometry snowflake3GPU;
+
 	CPU_Geometry squareDiamond;
 	GPU_Geometry squareDiamondGPU;
 
@@ -253,39 +292,8 @@ int main() {
 
 	std::vector<std::vector<float>> squareDiamondPoints{point1, point2, point3, point4, point1, point5, point6, point7, point8, point5};
 
-	// Initial points for Koch Snowflake
-	std::vector<float> kochStart{ 0.5, 0.f };
-	std::vector<float> kochEnd{ -0.5, 0.f };
-
-
-	// Serpinsky triangle generation
-	generateSerpinsky(first, second, third, triangles, 0);
-	serpinskyAllColored(triangles);
-
-	// Square Diamond generation, works best with GL_LINE_STRIP
-	generateSquareDiamond(squareDiamond, 3, squareDiamondPoints);
-	//colorAllVerts(squareDiamond, GREEN);
-
-	// Koch Snowflake generation
-	generateSnowflake(snowflake, kochStart, kochEnd, 1);
-	serpinskyAllColored(snowflake);
-
-	// Uploads data to GPU
-	trianglesGPU.setVerts(triangles.verts);
-	trianglesGPU.setCols(triangles.cols);
-
-	snowflakeGPU.setVerts(snowflake.verts);
-	snowflakeGPU.setCols(snowflake.cols);
-
-	squareDiamondGPU.setVerts(squareDiamond.verts);
-	squareDiamondGPU.setCols(squareDiamond.cols);
-
-	snowflakeGPU.setVerts(snowflake.verts);
-	snowflakeGPU.setCols(snowflake.cols);
-
-
-
 	State state;
+
 	// RENDER LOOP
 	while (!window.shouldClose()) {
 		glfwPollEvents();
@@ -294,10 +302,51 @@ int main() {
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		shader.use();
-		snowflakeGPU.bind();
-		glDrawArrays(GL_LINE_STRIP, 0, GLsizei(snowflake.verts.size()));
-		
 
+		if (!(state == callbacks->getState())) {
+			if (callbacks->getState().scene == 1) {
+				clearScene(triangles, squareDiamond, snowflake1, snowflake2, snowflake3);
+				generateSerpinsky(first, second, third, triangles, callbacks->getState().iterations);
+				serpinskyAllColored(triangles);
+				trianglesGPU.setVerts(triangles.verts);
+				trianglesGPU.setCols(triangles.cols);
+				trianglesGPU.bind();
+				glDrawArrays(GL_TRIANGLES, 0, GLsizei(triangles.verts.size()));
+			}
+			else if (callbacks->getState().scene == 2) {
+				clearScene(triangles, squareDiamond, snowflake1, snowflake2, snowflake3);
+				generateSquareDiamond(squareDiamond, callbacks->getState().iterations, squareDiamondPoints);
+				squareDiamondGPU.setVerts(squareDiamond.verts);
+				squareDiamondGPU.setCols(squareDiamond.cols);
+				squareDiamondGPU.bind();
+				glDrawArrays(GL_LINE_STRIP, 0, GLsizei(squareDiamond.verts.size()));
+			}
+			else if (callbacks->getState().scene == 3) {
+				clearScene(triangles, squareDiamond, snowflake1, snowflake2, snowflake3);
+				generateSnowflake(snowflake1, first, second, BLUE, callbacks->getState().iterations);
+				generateSnowflake(snowflake2, second, third, BLUE, callbacks->getState().iterations);
+				generateSnowflake(snowflake3, third, first, BLUE, callbacks->getState().iterations);
+
+				snowflake1GPU.setVerts(snowflake1.verts);
+				snowflake1GPU.setCols(snowflake1.cols);
+				snowflake1GPU.bind();
+				glDrawArrays(GL_LINE_STRIP, 0, GLsizei(snowflake1.verts.size()));
+
+				snowflake2GPU.setVerts(snowflake2.verts);
+				snowflake2GPU.setCols(snowflake2.cols);
+				snowflake2GPU.bind();
+				glDrawArrays(GL_LINE_STRIP, 0, GLsizei(snowflake2.verts.size()));
+
+				snowflake3GPU.setVerts(snowflake3.verts);
+				snowflake3GPU.setCols(snowflake3.cols);
+				snowflake3GPU.bind();
+				glDrawArrays(GL_LINE_STRIP, 0, GLsizei(snowflake3.verts.size()));
+				
+			}
+		}
+
+		
+		
 
 		glDisable(GL_FRAMEBUFFER_SRGB); // disable sRGB for things like imgui
 

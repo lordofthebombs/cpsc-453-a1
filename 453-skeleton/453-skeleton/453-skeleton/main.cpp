e96d8116bd83c88c2a714bb52e19e856a6512755
bader-waseem <25441886+bader-waseem@users.noreply.github.com>
2020-09-29 02:27:39 -0600

    Has a kind of working koch snowflake, will probably get rid of extra functions

--- 453-skeleton/453-skeleton/453-skeleton/main.cpp
+++ 453-skeleton/453-skeleton/453-skeleton/main.cpp
@@ -1,12 +1,13 @@
 #define _USE_MATH_DEFINES
-#include <cmath>
+#define GLM_SWIZZLE
+#include <math.h>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <vector>
-
-
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <iostream>
-
 #include "Geometry.h"
 #include "GLDebug.h"
 #include "Log.h"
@@ -75,10 +76,52 @@ glm::vec3 point(std::vector<float> point) {
 	return glm::vec3(point[0], point[1], 0.f);
 }
 
+// Point rotation referenced from https://www.geeksforgeeks.org/basic-transformations-opengl/#:~:text=To%20rotate%20around%20a%20different,x%2C%20y%2C%20z).
+std::vector<float> rotatePoint(std::vector<float> point, std::vector<float> pivot, float angle) {
+	int x = 0;
+	int y = 1;
+
+	float angleInRadians = angle * (float)(M_PI / 180.f);
+	float rotatedX = pivot[x] + (point[x] - pivot[x]) * cos(angleInRadians) - (point[y] - pivot[y]) * sin(angleInRadians);
+	float rotatedY = pivot[x] + (point[x] - pivot[x]) * sin(angleInRadians) + (point[y] - pivot[y])* cos(angleInRadians);
+
+	std::vector<float> newPoint{ rotatedX, rotatedY };
+	return newPoint;
+}
+
+// Gets a point in between p and q given an alpha value
+std::vector<float> pointOnLine(float alpha, std::vector<float> p, std::vector<float> q) {
+	int x = 0;
+	int y = 1;
+
+	float newX = (float)(1 - alpha) * p[x] + alpha * q[x];
+	float newY = (float)(1 - alpha) * p[y] + alpha * q[y];
+
+	std::vector<float> newPoint{ newX, newY };
+	return newPoint;
+}
+
+// Calculates vector made by two points
+std::vector<float> getVectorFromPoints(std::vector<float> p, std::vector<float> q) {
+	float firstElement = q[0] - p[0];
+	float secondElement = q[1] - p[1];
+	std::vector<float> vector{ firstElement, secondElement };
+	return vector;
+}
+
+// Moves a point along a vector
+std::vector<float> getPointFromVector(std::vector<float> point, glm::vec3 vector) {
+	float newX = point[0] + (float)vector.x;
+	float newY = point[1] + (float)vector.y;
+	std::vector<float> newPoint{ newX, newY };
+	return newPoint;
+}
+
 // Gets points for square diamond
 glm::vec3 squareDiamondPoint(std::vector<float> point, float factor) {
 	return glm::vec3((point[0] * factor), (point[1] * factor), 0.f);
 }
+
 
 // Gets midpoint of two vertices
 std::vector<float> midPoint(std::vector<float> vertex1, std::vector<float> vertex2) {
@@ -119,10 +162,50 @@ void generateSquareDiamond(CPU_Geometry& squareDiamond, int iterations, std::vec
 
 	for (int i = 0; i < iterations; i++) {
 		for (int j = 0; j < initialPoints.size(); j++) squareDiamond.verts.push_back(squareDiamondPoint(initialPoints[j], factor));
+		squareDiamond.cols.push_back(BLUE);
+		squareDiamond.cols.push_back(BLUE);
+		squareDiamond.cols.push_back(BLUE);
+		squareDiamond.cols.push_back(BLUE);
+		squareDiamond.cols.push_back(BLUE);
+		squareDiamond.cols.push_back(RED);
+		squareDiamond.cols.push_back(RED);
+		squareDiamond.cols.push_back(RED);
+		squareDiamond.cols.push_back(RED);
+		squareDiamond.cols.push_back(RED);
 		factor *= 0.5f;
 	}
 
 	
+}
+
+// Function to generate geometry for koch snowflake
+void generateSnowflake(CPU_Geometry& snowflake, std::vector<float> startingPoint, std::vector<float> endingPoint, int iterations) {
+	float firstPointAlpha = 1.f / 3.f;
+	float lastPointAlpha = 2.f / 3.f;
+	float endPointAlpha = 1.f;
+	if (iterations > 0) {
+		std::vector<float> firstPoint = pointOnLine(firstPointAlpha, startingPoint, endingPoint);
+		std::vector<float> lastPoint = pointOnLine(lastPointAlpha, startingPoint, endingPoint);
+		std::vector<float> firstLastVector = getVectorFromPoints(firstPoint, lastPoint);
+
+		glm::mat4 rotationMat(1); // Creates identity matrix
+
+		// Rotation referenced here https://stackoverflow.com/questions/20923232/how-to-rotate-a-vector-by-a-given-direction
+		rotationMat = glm::rotate(rotationMat, 60.0f, glm::vec3(firstLastVector[0], firstLastVector[1], 0.f));
+		glm::vec3 rotatedVector = glm::vec3(rotationMat * glm::vec4(glm::vec3(firstLastVector[0], firstLastVector[1], 0.f), 1.0));
+
+		std::vector<float> middle = getPointFromVector(firstPoint, rotatedVector);
+
+
+		generateSnowflake(snowflake, startingPoint, firstPoint, iterations - 1);
+		generateSnowflake(snowflake, firstPoint, middle, iterations - 1);
+		generateSnowflake(snowflake, middle, lastPoint, iterations - 1);
+		generateSnowflake(snowflake, lastPoint, endingPoint, iterations - 1);
+	}
+	else {
+		snowflake.verts.push_back(point(startingPoint));
+		snowflake.verts.push_back(point(endingPoint));
+	}
 }
 
 
@@ -170,16 +253,22 @@ int main() {
 
 	std::vector<std::vector<float>> squareDiamondPoints{point1, point2, point3, point4, point1, point5, point6, point7, point8, point5};
 
+	// Initial points for Koch Snowflake
+	std::vector<float> kochStart{ 0.5, 0.f };
+	std::vector<float> kochEnd{ -0.5, 0.f };
 
 
 	// Serpinsky triangle generation
 	generateSerpinsky(first, second, third, triangles, 0);
 	serpinskyAllColored(triangles);
 
-	// Square Diamond generation
-	generateSquareDiamond(squareDiamond, 5, squareDiamondPoints);
-	colorAllVerts(squareDiamond, GREEN);
+	// Square Diamond generation, works best with GL_LINE_STRIP
+	generateSquareDiamond(squareDiamond, 3, squareDiamondPoints);
+	//colorAllVerts(squareDiamond, GREEN);
 
+	// Koch Snowflake generation
+	generateSnowflake(snowflake, kochStart, kochEnd, 1);
+	serpinskyAllColored(snowflake);
 
 	// Uploads data to GPU
 	trianglesGPU.setVerts(triangles.verts);
@@ -190,6 +279,9 @@ int main() {
 
 	squareDiamondGPU.setVerts(squareDiamond.verts);
 	squareDiamondGPU.setCols(squareDiamond.cols);
+
+	snowflakeGPU.setVerts(snowflake.verts);
+	snowflakeGPU.setCols(snowflake.cols);
 
 
 
@@ -202,8 +294,8 @@ int main() {
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		shader.use();
-		squareDiamondGPU.bind();
-		glDrawArrays(GL_LINE_LOOP, 0, GLsizei(squareDiamond.verts.size()));
+		snowflakeGPU.bind();
+		glDrawArrays(GL_LINE_STRIP, 0, GLsizei(snowflake.verts.size()));
 		
 
 
